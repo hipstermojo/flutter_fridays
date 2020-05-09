3b7b72e42e3e90bd52782587c05db8c7e4348f59
Kenneth Gitere <gitere81@gmail.com>
2020-05-09 17:33:45 +0300

    Add readings of the scale and Montserrat bold font

--- humidity_slider/lib/main.dart
+++ humidity_slider/lib/main.dart
@@ -9,6 +9,11 @@ void main() {
 class Home extends StatelessWidget {
   final Color redGradient = Color(0xff560920);
   final Color blueGradient = Color(0xff2b6799);
+  final Color backgroundColor = Color(0xff173051);
+
+  final List<String> readings = [0, 10, 25, 30, 35, 40, 45, 50, 75, 100]
+      .map((reading) => reading.toString() + "%")
+      .toList();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,14 +23,56 @@ class Home extends StatelessWidget {
           textTheme: TextTheme(bodyText2: TextStyle(color: Colors.white)),
           iconTheme: IconThemeData(color: Colors.white.withAlpha(128))),
       home: Scaffold(
-        backgroundColor: Color(0xff173051),
+        backgroundColor: backgroundColor,
         body: SafeArea(
           child: Column(
             children: <Widget>[
               Expanded(
-                  child: Center(
-                child: Text("Scale goes here"),
-              )),
+                  child: Container(
+                      child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: readings
+                              .map((reading) => Container(
+                                    margin: EdgeInsets.only(bottom: 25.0),
+                                    child: Row(
+                                      children: <Widget>[
+                                        Container(
+                                            margin: EdgeInsets.only(
+                                                left: 10.0, right: 5.0),
+                                            decoration: BoxDecoration(
+                                                shape: BoxShape.circle,
+                                                color: Colors.yellow),
+                                            height: 4.0,
+                                            width: 4.0),
+                                        Text(
+                                          reading,
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ],
+                                    ),
+                                  ))
+                              .toList()
+                              .reversed
+                              .toList()),
+                    ),
+                    flex: 1,
+                  ),
+                  Expanded(
+                    child: Container(),
+                    flex: 1,
+                  ),
+                  Expanded(
+                    child: Container(),
+                    flex: 2,
+                  ),
+                ],
+              ))),
               BottomNav()
             ],
           ),
