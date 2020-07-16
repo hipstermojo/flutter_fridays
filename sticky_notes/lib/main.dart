34c60f25e41e98f851c84e5989f3f1f6741cdc19
Kenneth Gitere <gitere81@gmail.com>
2020-07-16 15:34:11 +0300

    Extract code rendering card into FoldableCard widget
    Remove dart:math import.

--- sticky_notes/lib/main.dart
+++ sticky_notes/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -22,56 +20,73 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color(0xFF465254),
         body: Center(
-          child: Stack(
-            overflow: Overflow.visible,
-            alignment: Alignment.center,
-            children: [
-              ClipPath(
-                clipper: CardClipper(
-                    startOffset: Offset(width - clipSize, 0),
-                    endOffset: Offset(width, clipSize)),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.pinkAccent,
-                      borderRadius: BorderRadius.circular(10.0)),
-                  height: height,
-                  width: width,
-                  alignment: Alignment.center,
-                  child: Container(
-                    padding: EdgeInsets.all(10.0),
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                            "Useful hints to build a perfect design for iPhone XS")
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                right: -width + clipSize,
-                top: -height + clipSize,
-                child: Transform.rotate(
-                  angle: 0,
-                  child: ClipPath(
-                    clipper: CardClipper(
-                        startOffset: Offset(0, height - clipSize),
-                        endOffset: Offset(clipSize, height)),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Colors.pink,
-                          borderRadius: BorderRadius.circular(10.0)),
-                      height: height,
-                      width: width,
-                      child: Text("Some text"),
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
+          child: FoldableCard(width: width, clipSize: clipSize, height: height),
         ),
       ),
+    );
+  }
+}
+
+class FoldableCard extends StatelessWidget {
+  const FoldableCard({
+    Key key,
+    @required this.width,
+    @required this.clipSize,
+    @required this.height,
+  }) : super(key: key);
+
+  final double width;
+  final double clipSize;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      overflow: Overflow.visible,
+      alignment: Alignment.center,
+      children: [
+        ClipPath(
+          clipper: CardClipper(
+              startOffset: Offset(width - clipSize, 0),
+              endOffset: Offset(width, clipSize)),
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.pinkAccent,
+                borderRadius: BorderRadius.circular(10.0)),
+            height: height,
+            width: width,
+            alignment: Alignment.center,
+            child: Container(
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                children: <Widget>[
+                  Text("Useful hints to build a perfect design for iPhone XS")
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          right: -width + clipSize,
+          top: -height + clipSize,
+          child: Transform.rotate(
+            angle: 0,
+            child: ClipPath(
+              clipper: CardClipper(
+                  startOffset: Offset(0, height - clipSize),
+                  endOffset: Offset(clipSize, height)),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.pink,
+                    borderRadius: BorderRadius.circular(10.0)),
+                height: height,
+                width: width,
+                child: Text("Some text"),
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
